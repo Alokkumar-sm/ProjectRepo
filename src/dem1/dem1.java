36b4ae3471cab0e7949635fefaed1ad3313c984d
Alokkumar-sm <alokauzz@gmail.com>
2023-11-07 14:27:29 +0530

    code pusshed

---
+++ src/dem1/dem1.java
@@ -0,0 +1,24 @@
+package dem1;
+
+public class dem1 {
+
+	
+	
+
+	static int x = 10;
+	static  int y = 20;
+	
+	public static void main(String[] args) {
+		
+		int z = x + y ;
+		
+		System.out.println(z);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		
+	} 
+	
+	
+	
+}
